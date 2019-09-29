6a92a2168379accf286af52edcc5f485f7e97e0d
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-09-29 14:33:55 +0800

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,7 +14,7 @@ const App = (props) => {
   return (
     <Router>
       <div>
-      <NavBar />
+        <NavBar />
         <Route exact path='/' component={Home}/>
         <Route exact path='/movies' component={Movies} />
         <Route exact path='/directors' component={Directors} />
