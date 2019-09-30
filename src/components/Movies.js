1e00330ce9668d37c03a50dd52c8443e3dabdce9
Brian <briandsalemi@gmail.com>
2019-09-30 11:51:50 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { movies } from '../data';
 
-const Movies = () => {
-  return (
-    <div>
-        {/*{code here}*/}
-    </div>
-  );
-};
+class Movies extends React.Component {
+
+  generateMovies(){
+    const movieComps = movies.map((movieObj) => {
+      return <div>
+        <h1>
+          Name:{movieObj.title}
+        </h1>
+        <p>
+          Time: {movieObj.time}
+        </p>
+          Genres: 
+              {movieObj.genres.forEach(movieGenre => { 
+              return <li>{movieGenre}</li>
+          })}
+      </div>
+    })
+    return movieComps
+  }
+  render(){
+    return(
+      <div>
+        <h1>Movies Page</h1>
+        {this.generateMovies()}
+      </div>
+    )
+  }
+}
 
 export default Movies;
