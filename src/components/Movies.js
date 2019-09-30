5872a3db914bc39607dea80be2c39409b0f40d1e
haleydek <hcd09@live.com>
2019-09-30 16:53:53 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (
+            <div>
+              <h3>{movie.title}</h3>
+              <p>{movie.time} minutes</p>
+              <ul>
+                {movie.genres.map(genre => {
+                  return <li>{genre}</li>
+                })}
+              </ul>
+            </div>
+          )
+        })}
     </div>
   );
 };
